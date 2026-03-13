710431228ded563bbe1ad415d0a093fb79ba4762
Sreeman <sreemanbitsmech@gmail.com>
2026-03-13 12:01:07 +0530

    components/data_ingestion.py & constant.py updated : Data Ingestion completed

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,66 @@
+# Read data from sources, split data
+import os, sys
+from src.logger import logging
+from src.exception import CustomException
+from src.constants import *
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+from dataclasses import dataclass
+
+
+# Data Ingestion Config : input
+@dataclass
+class Data_Ingestion_Config:
+    artifact_dir : str = os.path(ARTIFACT_DIR)
+    raw_data_path : str = os.path.join(ARTIFACT_DIR, RAW_DATA_FILENAME)
+    train_data_path : str = os.path.join(ARTIFACT_DIR, TRAIN_DATA_FiILENAME)
+    test_data_path : str = os.path.join(ARTIFACT_DIR, TEST_DATA_FILENAME)
+    
+
+# Data Ingestion Component
+class Data_Ingestion:
+    def __init__(self):
+        self.ingestion_config = Data_Ingestion_Config()
+    
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
+        try:
+            # read data
+            df = pd.read_csv(DATA_FILE_PATH)
+            logging.info("Read the dataset as a DataFrame")
+            
+            # create the directories
+            os.makedirs(self.ingestion_config.artifact_dir, exist_ok=True)
+            logging.info(f"Created artifacts directory: {self.ingestion_config.artifact_dir}")
+            
+            # save raw data
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            logging.info(f"Saved raw data in dir : {self.ingestion_config.raw_data_path}")
+            
+            # split the data
+            train_set, test_set = train_test_split(df, test_size=TEST_SIZE, random_state=42)
+
+            # save the train and test data
+            train_set.to_csv(self.ingestion_config.train_data_path)
+            logging.info(f"Saved train data in dir : {self.ingestion_config.train_data_path}")
+            test_set.to_csv(self.ingestion_config.test_data_path)
+            logging.info(f"Saved test data in dir : {self.ingestion_config.test_data_path}")
+            
+            logging.info(f"Inmgestion of the data is completed")
+            
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+
+if __name__ == "__main__":
+    obj=Data_Ingestion()
+    train_data_path,test_data_path = obj.initiate_data_ingestion()
+
+    print(f"Train Data Path : {train_data_path} \nTest Data Path : {test_data_path}")
---
+++ src/constants.py
@@ -0,0 +1,13 @@
+import os,sys
+
+
+ROOT_DIR = os.getcwd()
+
+# -------- Config --------
+# Data Ingestion Config
+ARTIFACT_DIR = 'artifact'
+RAW_DATA_FILENAME = 'raw_data'
+TRAIN_DATA_FiILENAME = 'train'
+TEST_DATA_FILENAME = 'test'
+DATA_FILE_PATH = 'notebook\data\StudentsPerformance.csv'
+TEST_SIZE = 0.2
