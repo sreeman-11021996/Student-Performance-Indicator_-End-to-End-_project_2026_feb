62a5cd541f42b49108140005a578713173212011
Sreeman <sreemanbitsmech@gmail.com>
2026-04-09 22:05:17 +0530

    model trainer component update

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -8,12 +8,8 @@ from collections import defaultdict
 from src.logger import logging
 from src.exception import CustomException
 from src.constants import *
-from src.utils import save_object, get_best_model_name, get_train_and_test_arrays
+from src.utils import save_object, get_best_model_name, get_train_and_test_arrays, evaluate_models
 from src.components.data_transformation import Data_Transformation_Artifact
-
-
-# metrics
-from sklearn.metrics import r2_score, mean_squared_error
 
 
 # algorithms
@@ -24,9 +20,6 @@ from sklearn.ensemble import (AdaBoostRegressor,GradientBoostingRegressor,Random
 from catboost import CatBoostRegressor
 from xgboost import XGBRegressor
 
-
-# training
-from sklearn.model_selection import KFold, cross_validate
 
 # data operation
 import numpy as np
@@ -58,81 +51,7 @@ class Model_Trainer:
         self.data_transformation_arifact = data_transformation_artifact
         self.model_trainer_config = Model_Trainer_Config()
       
-        
-    def evaluate_models(self, x_train:np.ndarray, y_train:np.ndarray, models:dict)-> Dict:
-        """
-        Arguments : models = {'linear_reg' : LinearRegression(), ...}
-        
-        Return : trained_models_report = {
-            'cval_r2_score' : {'linear_reg' : .., ...},
-            'overfit_gap' : {...},
-            'cval_r2_score_std' : ...
-            }
-        """
-        try:
-            # set up the report dictionary
-            # trained_models_report_fields = [CVAL_R2_SCORE, OVERFIT_GAP, CVAL_R2_SCORE_STD]
-            # trained_models_report = dict.fromkeys(trained_models_report_fields, {})
-            trained_models_report = defaultdict(lambda: defaultdict(float))
-            
-            logging.info(f"Starting the kfold cross validation on models : {models.keys()}")
-            # loop all the models
-            for model_name, model in models.items():
-                 
-                # kfold cross val
-                kf = KFold(n_splits=5, random_state=42, shuffle=True)
-                cval_r2_scores : list = []
-                train_r2_scores : list = []
                 
-                # get r2 scores using kfold validation for each model
-                for train_idx, val_idx in kf.split(x_train, y_train):
-                    
-                    # Split train into train_fold + val_fold
-                    x_train_fold, y_train_fold = x_train[train_idx], y_train[train_idx] 
-                    x_val_fold, y_val_fold = x_train[val_idx], y_train[val_idx]
-
-
-                    # Train on train_fold → predict on val_fold (test_r2)
-                    model.fit(x_train_fold, y_train_fold)
-                    y_val_pred = model.predict(x_val_fold)
-                    
-                    # Train on train_fold → predict on train_fold (train_r2)  
-                    y_train_pred = model.predict(x_train_fold)
-
-                
-                    # metrics - r2 scores calulation 
-                    cval_r2 = r2_score(y_val_fold, y_val_pred)
-                    train_r2 = r2_score(y_train_fold, y_train_pred)
-                    cval_r2_scores.append(cval_r2)
-                    train_r2_scores.append(train_r2)    
-                 
-                 
-                # get the metrics for the model report
-                cval_r2_mean = np.mean(cval_r2_scores)
-                train_r2_mean = np.mean(train_r2_scores)
-                overfit_gap = train_r2_mean - cval_r2_mean
-                cval_r2_std = np.std(cval_r2_scores)
- 
-                
-                # store metrics in model report
-                trained_models_report[CVAL_R2_SCORE][model_name] = float(cval_r2_mean)
-                trained_models_report[OVERFIT_GAP][model_name] = float(overfit_gap)
-                trained_models_report[CVAL_R2_SCORE_STD][model_name] = float(cval_r2_std)
-                
-                logging.info(f"\nmodel name : {model_name}"
-                             f"\ncval r2 mean : {cval_r2_mean}, {trained_models_report[CVAL_R2_SCORE][model_name]}"
-                             f"\noverfit gap : {overfit_gap}, {trained_models_report[OVERFIT_GAP][model_name]}"
-                             f"\ncval r2 std : {cval_r2_std}, {trained_models_report[CVAL_R2_SCORE_STD][model_name]}")  
-            
-            logging.info(f"Completed the training of models and generated the model report")    
-            return trained_models_report
-        
-        except Exception as e:
-            raise CustomException(e) from None
-        
-            
-        
-        
     def create_model_trainer_artifact(self, best_model:Any, trained_models:dict, best_model_name:str, 
                                       x_train:np.ndarray, y_train:np.ndarray, 
                                       x_test:np.ndarray, y_test: np.ndarray)-> Model_Trainer_Artifact:
@@ -185,6 +104,7 @@ class Model_Trainer:
             logging.info(f"start the loading of train and test arrays from artifacts dir")
             train_file_path = self.data_transformation_arifact.train_arr_file_path
             test_file_path = self.data_transformation_arifact.test_arr_file_path 
+            
             (x_train, y_train), (x_test, y_test) = get_train_and_test_arrays(
                                                     train_file_path=train_file_path, 
                                                     test_file_path=test_file_path)
@@ -205,7 +125,7 @@ class Model_Trainer:
             
             # evaluate the models dict
             logging.info(f"Start evaluation of all the models")
-            trained_models_report = self.evaluate_models(x_train=x_train, y_train=y_train, models=models)
+            trained_models_report = evaluate_models(x_train=x_train, y_train=y_train, models=models)
             
             # get best model name
             logging.info("Start the best model comparision")
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import os
 from typing import Any
+from collections import defaultdict
 
 from src.exception import CustomException
 from src.logger import logging
@@ -9,6 +10,10 @@ from src.constants import *
 import pandas as pd
 import numpy as np
 import dill
+
+# metrics
+from sklearn.metrics import r2_score
+from sklearn.model_selection import KFold, cross_validate
 
 
 
@@ -101,7 +106,7 @@ def load_numpy_array_data(file_path:str)-> np.ndarray:
         raise CustomException(e) from None
     
 
-def get_train_and_test_arrays(train_file_path:str, test_file_path:str)->tuple:
+def get_train_and_test_arrays (train_file_path:str, test_file_path:str)->tuple:
     """
     Returns:
     tuple: (x_train, y_train), (x_test, y_test) : all are np.ndarrays
@@ -120,8 +125,78 @@ def get_train_and_test_arrays(train_file_path:str, test_file_path:str)->tuple:
         raise CustomException(e) from None
 
     
+def evaluate_models(x_train:np.ndarray, y_train:np.ndarray, models:dict)-> Dict:
+    """
+    Arguments : models = {'linear_reg' : LinearRegression(), ...}
+        
+    Return : trained_models_report = {
+        'cval_r2_score' : {'linear_reg' : .., ...},
+        'overfit_gap' : {...},
+        'cval_r2_score_std' : ...
+        }
+    """
+    try:
+        # set up the report dictionary
+        trained_models_report = defaultdict(lambda: defaultdict(float))
+            
+        logging.info(f"Starting the kfold cross validation on models : {models.keys()}")
+        # loop all the models
+        for model_name, model in models.items():
+                 
+            # kfold cross val
+            kf = KFold(n_splits=5, random_state=42, shuffle=True)
+            cval_r2_scores : list = []
+            train_r2_scores : list = []
+                
+            # get r2 scores using kfold validation for each model
+            for train_idx, val_idx in kf.split(x_train, y_train):
+                    
+                # Split train into train_fold + val_fold
+                x_train_fold, y_train_fold = x_train[train_idx], y_train[train_idx] 
+                x_val_fold, y_val_fold = x_train[val_idx], y_train[val_idx]
 
-def get_best_model_name(trained_models:dict, base_r2_score= BASE_R2_SCORE, 
+
+                # Train on train_fold → predict on val_fold (test_r2)
+                model.fit(x_train_fold, y_train_fold)
+                y_val_pred = model.predict(x_val_fold)
+                    
+                # Train on train_fold → predict on train_fold (train_r2)  
+                y_train_pred = model.predict(x_train_fold)
+
+                
+                # metrics - r2 scores calulation 
+                cval_r2 = r2_score(y_val_fold, y_val_pred)
+                train_r2 = r2_score(y_train_fold, y_train_pred)
+                cval_r2_scores.append(cval_r2)
+                train_r2_scores.append(train_r2)    
+                 
+                 
+            # get the metrics for the model report
+            cval_r2_mean = np.mean(cval_r2_scores)
+            train_r2_mean = np.mean(train_r2_scores)
+            overfit_gap = train_r2_mean - cval_r2_mean
+            cval_r2_std = np.std(cval_r2_scores)
+ 
+                
+            # store metrics in model report
+            trained_models_report[CVAL_R2_SCORE][model_name] = float(cval_r2_mean)
+            trained_models_report[OVERFIT_GAP][model_name] = float(overfit_gap)
+            trained_models_report[CVAL_R2_SCORE_STD][model_name] = float(cval_r2_std)
+                
+            logging.info(f"\nmodel name : {model_name}"
+                         f"\ncval r2 mean : {cval_r2_mean}, {trained_models_report[CVAL_R2_SCORE][model_name]}"
+                         f"\noverfit gap : {overfit_gap}, {trained_models_report[OVERFIT_GAP][model_name]}"
+                         f"\ncval r2 std : {cval_r2_std}, {trained_models_report[CVAL_R2_SCORE_STD][model_name]}")  
+            
+        logging.info(f"Completed the training of models and generated the model report")    
+        return trained_models_report
+        
+    except Exception as e:
+        raise CustomException(e) from None
+
+
+
+def get_best_model_name (trained_models:dict, base_r2_score= BASE_R2_SCORE, 
                        base_overfit_gap= BASE_OVERFIT_GAP)-> str:
     """
     Arguments : trained_models = {
